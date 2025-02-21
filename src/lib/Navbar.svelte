ec08414e5596bcbe775297dd0e90af3345038524
collincollins <collinwcollins@gmail.com>
2025-02-20 22:12:09 -0500

    version 0

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -14,32 +14,32 @@
   <div class="absolute top-[-10rem] left-[-8rem] w-[30rem] h-[30rem]">
     <Cloud3D />
     <div class="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
-      <img src={logo} alt="Aerogen Logo" class="h-16 w-auto" />
+      <img src={logo} alt="Aerogen Logo" class="h-16 w-auto mt-4" />
     </div>
   </div>
 
   <!-- Hamburger Button at Top Right -->
-  <div class="absolute top-4 right-4">
+  <!-- <div class="absolute top-4 right-4">
     <button 
       on:click={toggleMenu}
       class="p-2 focus:outline-none"
       aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
-      {#if isMenuOpen}
+      {#if isMenuOpen} -->
         <!-- White X Icon -->
-        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <!-- <svg class="h-8 w-8 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
-      {:else}
+      {:else} -->
         <!-- White Hamburger Icon -->
-        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <!-- <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       {/if}
     </button>
-  </div>
+  </div> -->
 
   <!-- Sliding Panel from the Right -->
-  {#if isMenuOpen}
+  <!-- {#if isMenuOpen}
     <div class="absolute top-0 right-0 h-screen w-64 bg-white shadow-lg p-4 transition-transform duration-300">
       <nav>
         <ul>
@@ -51,5 +51,5 @@
         </ul>
       </nav>
     </div>
-  {/if}
+  {/if} -->
 </header> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
   // Page-specific logic (if any)
 </script>
 
-<div class="min-h-screen">
-  <!-- Page content goes here -->
+<div class="min-h-screen flex items-center justify-center">
+  <!-- White card with glow -->
+  <div class="bg-white rounded-lg px-16 py-12 shadow-[0_0_10px_rgba(255,255,255,1)] flex items-center justify-center">
+    <h1 class="text-xl font-light text-primary font-aileron">
+      Aerogen Inc.
+    </h1>
+  </div>
 </div>
